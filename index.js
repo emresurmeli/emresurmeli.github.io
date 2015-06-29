f5ce774695d96ca560101b025f5834af80da26af
Emre Surmeli <s.emre.s.8@gmail.com>
2015-06-29 13:21:26 -0700

    first bundle

--- index.js
+++ index.js
@@ -1,65 +1,77 @@
-// React dependencies
-var React  = require('react/addons');
-var Router = require('react-router-component');
+/**
+ * @jsx React.DOM
+ */
 
-// Pull React animation component and Router components
+var React              = require('react/addons');
 var CSSTransitionGroup = React.addons.CSSTransitionGroup;
+var Router             = require('react-router-component');
 var Location           = Router.Location;
 var Link               = Router.Link;
 
-// Animated location routes
 var AnimatedLocations = React.createClass({
-  mixins: [Router.RouterMixin, Router.AsyncRouterRenderingMixin],
 
-  getRoutes: function() {
-    return this.props.children;
-  },
+    mixins: [Router.RouterMixin, Router.AsyncRouteRenderingMixin],
 
-  render: function() {
+    getRoutes: function() {
+      return this.props.children;
+    },
 
-    var handler    = this.renderRouterHandler();
-    var isPopState = this.state.navigation.isPopState;
-    var enabled    = isPopState ?
-                     !!this.props.popStateTransitionName :
-                     !!this.state.navigation.noTransition;
-    var props = {
-      component: React.DOM.div,
-      transitionEnter: enabled,
-      transitionLeave: enabled,
-    };
+    render: function() {
 
-    if(isPopState && this.props.popStateTransitionName) {
-      props.transitionName = this.props.popStateTransitionName;
-    } else if(this.state.navigation.transitionName) {
-      props.transitionName = this.state.navigation.transitionName;
+      var handler = this.renderRouteHandler();
+      var isPopState = this.state.navigation.isPopState;
+      var enabled = isPopState ?
+                    !!this.props.popStateTransitionName :
+                    !this.state.navigation.noTransition;
+      var props = {
+        component: React.DOM.div,
+        transitionEnter: enabled,
+        transitionLeave: enabled,
+      };
+      if (isPopState && this.props.popStateTransitionName) {
+        props.transitionName = this.props.popStateTransitionName;
+      } else if (this.state.navigation.transitionName) {
+        props.transitionName = this.state.navigation.transitionName;
+      }
+
+      handler.props.key = this.state.match.path;
+      return this.transferPropsTo(CSSTransitionGroup(props, handler));
+
     }
-
-    handler.props.key = this.state.match.path;
-    return this.transferPropsTo(CSSTransitionGroup(props, handler));
-  }
 });
 
-// Set Parent component with AnimatedLocation Routes
 var App = React.createClass({
   render: function() {
     return (
       <AnimatedLocations hash className="Main" transitionName="moveUp" popStateTransitionName="fade">
-        <Location path="/" handler={HomePage} />
+        <Location path="/" handler={MainPage} />
         <Location path="/about" handler={AboutPage} />
       </AnimatedLocations>
     )
   }
 })
 
-// Home Page
-var HomePage = React.createClass({
+var MainPage = React.createClass({
   render: function() {
     return (
       <div className="MainPage Page">
         <div className="Page__wrapper">
-          <h1>Emre Surmeli</h1>
+          <h1>Main page</h1>
           <p>
-            <Link href="/about">About</Link>
+            This demo shows how to do <a href={githubHref}>animated page
+            transitions</a> with <a href={rrcHref}>React Router component</a>.
+          </p>
+          <p>
+            All you need is to use <code>AnimatedLocations</code> router:
+          </p>
+          <pre>{[
+            '<AnimatedLocations className="Main" transitionName="moveUp" popStateTransitionName="fade">',
+            '  <Location path="/" handler={MainPage} />',
+            '  <Location path="/about" handler={AboutPage} />',
+            '</AnimatedLocations>'
+          ].join('\n')}</pre>
+          <p>
+            Now, click on a link to go to <Link href="/about">about page</Link>.
           </p>
         </div>
       </div>
@@ -67,18 +79,50 @@ var HomePage = React.createClass({
   }
 })
 
-// About Page
 var AboutPage = React.createClass({
   render: function() {
     return (
-      <div className="AboutPage">
+      <div className="AboutPage Page">
         <div className="Page__wrapper">
-          <h1>About Emre</h1>
-          <Link transitionName="moveDown" href="/">Home</Link>
+          <h1>About</h1>
+          <p>Then specify your CSS3 Transition in stylesheet:</p>
+          <pre>{[
+            '.moveUp-enter {',
+            '  ...',
+            '}',
+            '',
+            '.moveUp-enter.moveUp-enter-active {',
+            '  ...',
+            '}',
+            '',
+            '.moveUp-leave {',
+            '  ...',
+            '}',
+            '',
+            '.moveUp-leave.moveUp-leave-active {',
+            '  ...',
+            '}',
+          ].join('\n')}</pre>
+          <p>
+            Go back to <Link transitionName="moveDown" href="/">main page
+            </Link> (please notice it uses another kind of animated transition).
+          </p>
+          <p>
+            This link will lead to <Link noTransition transitionName="moveDown" href="/">main page
+            </Link> too but with no animated transition.
+          </p>
+          <p>
+            You can also use the back/forward buttons in your browser. <br />
+            Back/forward actions will use the <code>popStateTransitionName</code> specified in the props. <br />
+            It is set to a fade transition in this demo. If none is specified, no animation will be used.
+          </p>
         </div>
       </div>
     )
   }
 })
+
+var rrcHref = "http://andreypopp.viewdocs.io/react-router-component";
+var githubHref = "https://github.com/andreypopp/react-router-page-transition";
 
 React.renderComponent(App(), document.body);
